0032e664ce8123014fc262b345ca5ca25c4b8186
Oscar Andres Hernandez Ariza <Oscar Andres Hernandez Ariza>
2016-01-31 21:50:24 -0500

    Suse V2.0

--- Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -16,6 +16,8 @@ public class Ejercicio5 {
      */
     public static void main(String[] args) {
         System.out.println("Soy linux V2.0");
+        
+        System.out.println("Dristribucion Suse V2.0");
                 
     }
     
