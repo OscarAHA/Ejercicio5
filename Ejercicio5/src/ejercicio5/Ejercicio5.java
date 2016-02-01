32e9f8388a5940925752399ad1cc868ac937962d
Oscar Andres Hernandez Ariza <Oscar Andres Hernandez Ariza>
2016-01-31 21:47:04 -0500

    Linux V2.0

--- Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -15,7 +15,7 @@ public class Ejercicio5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Soy linux V1.0");
+        System.out.println("Soy linux V2.0");
                 
     }
     
